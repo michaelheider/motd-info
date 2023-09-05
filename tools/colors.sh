a6338f5d254a97525b98cfbe7fe2760971c0a2d4
Michael Heider <michael@heider.org>
2023-09-05 17:29:21 +0200

    set -euo pipefail

--- tools/colors.sh
+++ tools/colors.sh
@@ -1,58 +1,64 @@
-#!!/bin/bash
+#!/bin/bash
+set -euo pipefail
 
-# Reset
-reset="\e[0m"           # Text Reset
+# Define colors and conditional coloring functions.
+
+# reset
+reset="\e[0m"           # reset style
 
 # regular colors
 black="\e[0;30m"        # black
-red="\e[0;31m"          # Red
-green="\e[0;32m"        # Green
-yellow="\e[0;33m"       # Yellow
+red="\e[0;31m"          # red
+green="\e[0;32m"        # green
+yellow="\e[0;33m"       # yellow
 blue="\e[0;34m"         # blue
-purple="\e[0;35m"       # Purple
-cyan="\e[0;36m"         # Cyan
-white="\e[0;37m"        # White
+purple="\e[0;35m"       # purple
+cyan="\e[0;36m"         # cyan
+white="\e[0;37m"        # white
 
-# bold
-bblack="\e[1;30m"       # Black
-bred="\e[1;31m"         # Red
-bgreen="\e[1;32m"       # Green
-byellow="\e[1;33m"      # Yellow
-bblue="\e[1;34m"        # Blue
-bpurple="\e[1;35m"      # Purple
-bcyan="\e[1;36m"        # Cyan
-bwhite="\e[1;37m"       # White
+# bold colors
+bblack="\e[1;30m"       # black
+bred="\e[1;31m"         # red
+bgreen="\e[1;32m"       # green
+byellow="\e[1;33m"      # yellow
+bblue="\e[1;34m"        # blue
+bpurple="\e[1;35m"      # purple
+bcyan="\e[1;36m"        # cyan
+bwhite="\e[1;37m"       # white
+
 
 # c_if '2' '<' '5' '%'
 c_if(){
-    if (( ${1%%.*} $2 ${3%%.*} )); then
-        echo "${bgreen}$1${@:4}${reset}"
-    else
-        echo "${bred}$1${@:4}${reset}"
-    fi
+	if (( "${1%%.*}" "$2" "${3%%.*}" )); then
+		echo "${bgreen}$1${*:4}${reset}"
+	else
+		echo "${bred}$1${*:4}${reset}"
+	fi
 }
-# c_if '2' '<' '5' 'r1' 'r2'
+
+# c_if '2' '<' '5' 'r1' 'r2'adamshand
 c_if_r(){
-    if (( ${1%%.*} $2 ${3%%.*} )); then
-        echo "${bgreen}${4}${reset}"
-    else
-        echo "${bred}${5}${reset}"
-    fi
+	if (( ${1%%.*} "$2" "${3%%.*}" )); then
+		echo "${bgreen}${4}${reset}"
+	else
+		echo "${bred}${5}${reset}"
+	fi
 }
+
 # c_match 'abc' 'def' 'pkgs'
 c_match(){
-    if [[ ${1%%.*} = ${2%%.*} ]]; then
-        echo "${bgreen}$1${@:3}${reset}"
-    else
-        echo "${bred}$1${@:3}${reset}"
-    fi
+	if [[ ${1%%.*} = "${2%%.*}" ]]; then
+		echo "${bgreen}$1${*:3}${reset}"
+	else
+		echo "${bred}$1${*:3}${reset}"
+	fi
 }
 
 # c_match_r 'abc' 'def' 'r1' 'r2'
 c_match_r(){
-    if [[ ${1%%.*} = ${2%%.*} ]]; then
-        echo "${bgreen}${3}${reset}"
-    else
-        echo "${bred}${4}${reset}"
-    fi
+	if [[ ${1%%.*} = "${2%%.*}" ]]; then
+		echo "${bgreen}${3}${reset}"
+	else
+		echo "${bred}${4}${reset}"
+	fi
 }
