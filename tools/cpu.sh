4d9a8491302898eb512ca22bfb0059340d26f991
Michael Heider <michael@heider.org>
2023-09-05 17:27:42 +0200

    rewrite to be more efficient

--- tools/cpu.sh
+++ tools/cpu.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
-renice -n 20 -p "$$" &>/dev/null
+set -euo pipefail
 
-iter=2
-delay=0.5
+# Output CPU usage in percent.
+# Includes a noticeable sleep time.
 
-# 0     1       2    3       4          5      6   7       8     9     10
-#       user    nice system  idle       iowait irq softirq steal guest guest_nice
-# cpu   180288  509  316177  443927540  53229  0   5395    0     0     0
+# return idle and total CPU time since system start
+cpuStats() {
+	read -r -a cpu <<<"$(grep '^cpu ' /proc/stat)"
+	idle=$((cpu[4] + cpu[5]))
+	unset 'cpu[0]'
+	total=$((${cpu[@]/%/+}0))
+	echo "$idle $total"
+}
 
-# init
-previous_idle=0
-previous_total=0
-for i in $(seq "${iter}"); do
-        cpu=( $(grep '^cpu ' /proc/stat) )
-        unset cpu[0]
-        # compute idle cpu time
-        idle=$(( ${cpu[4]} + ${cpu[5]} ))
-        # compute total cpu time
-        total=$(( ${cpu[@]/%/+}0 ))
-        # compute cpu usage since last check
-        idle_d=$(( idle - previous_idle ))
-        total_d=$(( total - previous_total ))
-        cpu_usage=$(( 100*(total_d-idle_d)/total_d ))
-        # remember cpu total and idle for next check
-        previous_total="${total}"
-        previous_idle="${idle}"
-        # wait before next check
-        sleep "${delay}"
-done
-echo "${cpu_usage}" # %
+read -r -a start <<<"$(cpuStats)"
+sleep 0.5 # making this longer gives a better average
+read -r -a end <<<"$(cpuStats)"
 
+idle=$((end[0] - start[0]))
+total=$((end[1] - start[1]))
+
+cpuUsage=$((100 * (total - idle) / total)) # in percent
+
+echo "${cpuUsage}"
