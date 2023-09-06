1a6d049a4c8771a957f7cf3487595e4b9ee24155
Michael Heider <michael@heider.org>
2023-09-06 02:03:48 +0200

    improve colors.sh

--- tools/colors.sh
+++ tools/colors.sh
@@ -4,27 +4,32 @@ set -euo pipefail
 # Define colors and conditional coloring functions.
 
 # reset
-reset="\e[0m"           # reset style
+reset="\e[0m"
 
 # regular colors
-black="\e[0;30m"        # black
-red="\e[0;31m"          # red
-green="\e[0;32m"        # green
-yellow="\e[0;33m"       # yellow
-blue="\e[0;34m"         # blue
-purple="\e[0;35m"       # purple
-cyan="\e[0;36m"         # cyan
-white="\e[0;37m"        # white
+black="\e[0;30m"      
+red="\e[0;31m"         
+green="\e[0;32m"      
+yellow="\e[0;33m"    
+blue="\e[0;34m"        
+purple="\e[0;35m"     
+cyan="\e[0;36m"        
+white="\e[0;37m"       
 
 # bold colors
-bblack="\e[1;30m"       # black
-bred="\e[1;31m"         # red
-bgreen="\e[1;32m"       # green
-byellow="\e[1;33m"      # yellow
-bblue="\e[1;34m"        # blue
-bpurple="\e[1;35m"      # purple
-bcyan="\e[1;36m"        # cyan
-bwhite="\e[1;37m"       # white
+bblack="\e[1;30m"     
+bred="\e[1;31m"      
+bgreen="\e[1;32m"     
+byellow="\e[1;33m"     
+bblue="\e[1;34m"     
+bpurple="\e[1;35m"   
+bcyan="\e[1;36m"      
+bwhite="\e[1;37m"    
+
+# other
+clearLine="\e[K"
+oneLineUp="\e[1A" # can replace 1 with a higher integer
+twoLinesUp="\e[2A"
 
 
 # c_if '2' '<' '5' '%'
@@ -38,7 +43,7 @@ c_if(){
 
 # c_if '2' '<' '5' 'r1' 'r2'adamshand
 c_if_r(){
-	if (( ${1%%.*} "$2" "${3%%.*}" )); then
+	if (( "${1%%.*}" "$2" "${3%%.*}" )); then
 		echo "${bgreen}${4}${reset}"
 	else
 		echo "${bred}${5}${reset}"
@@ -47,7 +52,7 @@ c_if_r(){
 
 # c_match 'abc' 'def' 'pkgs'
 c_match(){
-	if [[ ${1%%.*} = "${2%%.*}" ]]; then
+	if [[ "${1%%.*}" = "${2%%.*}" ]]; then
 		echo "${bgreen}$1${*:3}${reset}"
 	else
 		echo "${bred}$1${*:3}${reset}"
@@ -56,7 +61,7 @@ c_match(){
 
 # c_match_r 'abc' 'def' 'r1' 'r2'
 c_match_r(){
-	if [[ ${1%%.*} = "${2%%.*}" ]]; then
+	if [[ "${1%%.*}" = "${2%%.*}" ]]; then
 		echo "${bgreen}${3}${reset}"
 	else
 		echo "${bred}${4}${reset}"
