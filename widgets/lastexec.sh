d49a80b94c0d5bbdf72a27c7291671a9de75584f
Michael Heider <michael@heider.org>
2023-09-08 01:06:13 +0200

    remove second accuracy

--- widgets/lastexec.sh
+++ widgets/lastexec.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # Prints the current time, i.e. the time of execution.
 # Run it to see what it looks like.
 
-echo "as of $(date +"%Y-%m-%d %H:%M:%S %Z")"
+echo "as of $(date +"%Y-%m-%d %H:%M %Z")"
