a7504625457106f44453c8bc8b4c3f67b6920200
Michael Heider <michael@heider.org>
2023-09-07 04:16:41 +0200

    refactor

--- tools/colors.sh
+++ tools/colors.sh
@@ -4,38 +4,39 @@ set -euo pipefail
 # Define colors and conditional coloring functions.
 
 # reset
-RESET="\e[0m"
+RESET='\e[0m'
 
 # regular colors
-BLACK="\e[0;30m"      
-RED="\e[0;31m"         
-GREEN="\e[0;32m"      
-YELLOW="\e[0;33m"    
-BLUE="\e[0;34m"        
-PURPLE="\e[0;35m"     
-CYAN="\e[0;36m"        
-WHITE="\e[0;37m"       
+BLACK='\e[0;30m'
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+YELLOW='\e[0;33m'
+BLUE='\e[0;34m'
+PURPLE='\e[0;35m'
+CYAN='\e[0;36m'
+WHITE='\e[0;37m'
 
 # bold colors
-BLACK_B="\e[1;30m"     
-RED_B="\e[1;31m"      
-GREEN_B="\e[1;32m"     
-YELLOW_B="\e[1;33m"     
-BLUE_B="\e[1;34m"     
-PURPLE_B="\e[1;35m"   
-CYAN_B="\e[1;36m"      
-WHITE_B="\e[1;37m"    
-
-# other
-LINE_CLEAR="\e[K"
-LINE_UP="\e[1A" # can replace 1 with a higher integer
-LINE_UP2="\e[2A"
+BLACK_B='\e[1;30m'
+RED_B='\e[1;31m'
+GREEN_B='\e[1;32m'
+YELLOW_B='\e[1;33m'
+BLUE_B='\e[1;34m'
+PURPLE_B='\e[1;35m'
+CYAN_B='\e[1;36m'
+WHITE_B='\e[1;37m'
 
 # semantic colors
 COLOR_GOOD=$GREEN
 COLOR_BAD=$RED_B
 COLOR_INFO=$YELLOW
 
+# other
+LINE_CLEAR='\e[K'
+LINE_UP='\e[1A'
+LINE_UP2='\e[2A' # can also do higher numbers
+
+# conditional coloring functions
 
 # example use: colorIf '2' '<' '5' '%'
 colorIf() {
