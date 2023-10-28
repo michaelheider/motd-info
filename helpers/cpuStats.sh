2e78b2f7eaba952c95a7669b74050fdf6e0ee9da
Michael Heider <michael@heider.org>
2023-10-28 19:25:09 +0200

    sysinfo reports disk IO

--- helpers/cpuStats.sh
+++ helpers/cpuStats.sh
@@ -11,10 +11,11 @@ source "${HELPERS}/colors.sh"
 # return idle and total CPU time since system start
 cpuStats() {
 	read -r -a cpu <<<"$(grep '^cpu ' /proc/stat)"
-	idle=$((cpu[4] + cpu[5]))
+	iowait=${cpu[5]}
+	idle=$((cpu[4] + cpu[5])) # idle + iowait
 	unset 'cpu[0]'
 	total=$((${cpu[@]/%/+}0))
-	echo "$idle $total"
+	echo "$idle $iowait $total"
 }
 
 read -r -a start <<<"$(cpuStats)"
@@ -23,9 +24,11 @@ sleep 0.5 # making this longer gives a better average
 echo -e "$LINE_UP$LINE_CLEAR$LINE_UP" >&2 # clear previous message
 read -r -a end <<<"$(cpuStats)"
 
+iowait=$((end[1] - start[1]))
 idle=$((end[0] - start[0]))
-total=$((end[1] - start[1]))
+total=$((end[2] - start[2]))
 
 cpuUsage=$((100 * (total - idle) / total)) # in percent
+ioDelay=$((100 * iowait / total)) # in percent
 
-echo "${cpuUsage}"
+echo "$cpuUsage $ioDelay"
--- widgets/sysinfo.sh
+++ widgets/sysinfo.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 
 # config: cutoff values
 CPU_WARN=30  # %
+IO_DELAY_WARN=30  # % (percent of time CPU is explicitly waiting for IO)
 MEM_WARN=50  # %
 SWAP_WARN=50 # %
 TEMP_WARN=60 # °C
@@ -15,21 +16,24 @@ HELPERS=$(realpath "$(dirname "$0")/../helpers")
 source "${HELPERS}/colors.sh"
 
 processes="$(ps --ppid 2 -p 2 --deselect | wc -l)"
-load="$(cut -d' ' -f3 </proc/loadavg)"
+load="$(cut -d' ' -f1 </proc/loadavg)"
 users="$(w -h | wc -l)"
 uptime="$(($(cut -d'.' -f1 </proc/uptime) / 3600 / 24))"
 mem="$(free -b | grep 'Mem' | awk '{ p=100*$3/$2; printf("%0.f",p) }')"
 # if there is no swap space, it ouputs '-'
 swap="$(free -b | grep 'Swap' | awk '{ if($2!=0) { p=100*$3/$2; printf("%0.f",p) } else { print "-" } }')"
 cores="$(grep -c '^processor' /proc/cpuinfo)"
-cpu="$("${HELPERS}/cpu.sh")"
+read -r -a cpuStats <<<"$("${HELPERS}/cpuStats.sh")"
+cpuUsage=${cpuStats[0]}
+ioDelay=${cpuStats[1]}
 if [[ -f '/sys/class/thermal/thermal_zone0/temp' ]]; then
 	temp="$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))"
 else
 	temp="?"
 fi
 
-cpu=$(colorIf "${cpu}" '<' "${CPU_WARN}" '%')
+cpuUsage=$(colorIf "${cpuUsage}" '<' "${CPU_WARN}" '%')
+ioDelay=$(colorIf "${ioDelay}" '<' "${IO_DELAY_WARN}" '%')
 mem=$(colorIf "${mem}" '<' "${MEM_WARN}" '%')
 if [[ $swap != '-' ]]; then
 	swap=$(colorIf "${swap}" '<' "${SWAP_WARN}" '%')
@@ -38,10 +42,11 @@ temp=$(colorIf "${temp}" '<' "${TEMP_WARN}" '°C')
 load=$(colorIf "${load}" '<' "$cores")
 
 table=''
-table+="CPU|${cpu}|sys load|${load}\n"
-table+="memory|${mem}|processes|${processes}\n"
-table+="swap|${swap}|uptime|${uptime}d\n"
-table+="temp|${temp}|users now|${users}\n"
+table+="CPU use|${cpuUsage}|IO delay|${ioDelay}\n"
+table+="memory|${mem}|swap|${swap}\n"
+table+="processes|${processes}|sys load|${load}\n"
+table+="uptime|${uptime}d|temp|${temp}\n"
+table+="users now|${users}\n"
 
 echo 'system resources:'
 echo -e "${table}" | column -ts'|' -R "2,4" | sed 's,^,  ,'
